bf0f4a1575364d1e5fb324f7a65e6dc060ecdd1c
Flower1111 <kryva.liliia@gmail.com>
2022-01-26 00:44:43 +0200

    Create "todo-list" React project

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React, {Fragment} from "react";
+import { NavLink } from "react-router-dom";
+
+export const Navbar = () => (
+    <nav className="navbar navbar-dark navbar-expand-lg bg-primary">
+        <div className="navbar-brand">
+            Note App
+        </div>
+        <ul className="navbar-nav">
+            <li className="nav-item">
+                <NavLink 
+                className="nav-link"
+                to="/"
+                exact
+                >
+                    Main page
+                </NavLink>
+            </li>
+            <li className="nav-item">
+                <NavLink 
+                className="nav-link"
+                to="/about"
+                >
+                    Information
+                </NavLink>
+            </li>
+      </ul>
+    </nav>
+)
